a283562fad4942f98c646d6a24905b3df08ccbf5
AppleNinjaHawk <wannybob1@gmail.com>
2024-08-07 21:22:26 -0700

    Update submission_v16.py

--- competition_code/new_code/submission_v16.py
+++ competition_code/new_code/submission_v16.py
@@ -9,7 +9,7 @@ from typing import List, Tuple, Dict, Optional
 import math
 import numpy as np
 import roar_py_interface
-
+print("hi")
 
 def normalize_rad(rad : float):
     return (rad + np.pi) % (2 * np.pi) - np.pi
